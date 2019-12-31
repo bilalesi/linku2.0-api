1ff84bbd1ddb771f9c07f141dbe508efb4ba4888
krthr <twilson@uninorte.edu.co>
2019-12-31 00:23:20 -0500

    scraper baseURL config

--- src/config/index.js
+++ src/config/index.js
@@ -2,9 +2,16 @@ module.exports = {
   server: {
     hostname: process.env.HOSTNAME,
     port: process.env.PORT,
-    secret: process.env.SECRET,
+    secret: process.env.SECRET
   },
+
   database: {
-    url: process.env.DATABASE_URL,
+    url: process.env.DATABASE_URL
   },
+
+  scraper: {
+    baseURL: "https://guayacan.uninorte.edu.co/4PL1CACI0N35/registro"
+  },
+
+  actualPeriod: process.env.ACTUAL_PERIOD || "202010"
 };
