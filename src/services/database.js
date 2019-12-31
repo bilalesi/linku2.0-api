63d3c74ba636aa6fb591cbf8782e971b41237c80
krthr <twilson@uninorte.edu.co>
2019-12-31 00:23:04 -0500

    database service

--- src/services/database.js
+++ src/services/database.js
@@ -1,31 +1,31 @@
-const mongoose = require('mongoose');
-const config = require('../config');
+const mongoose = require("mongoose");
+const config = require("../config");
 
 const { database } = config;
 
 /**
  * Connect to database
  */
-const connect = () => {
-  mongoose.connect(database.url, { useNewUrlParser: true }, (err) => {
+function connect() {
+  mongoose.connect(database.url, { useNewUrlParser: true }, err => {
     if (err) {
-      console.log('Database connection error:', err);
+      console.log("Database connection error:", err);
     }
   });
 
-  mongoose.connection.on('open', () => {
-    console.log(`Database => ${database.url} \x1b[32m%s\x1b[0m`, 'connected');
+  mongoose.connection.on("open", () => {
+    console.log(`Database => ${database.url} \x1b[32m%s\x1b[0m`, "connected");
   });
-};
+}
 
 /**
  * Disconnect to database
  */
-const disconnect = () => {
+function disconnect() {
   mongoose.connection.close();
-};
+}
 
 module.exports = {
   connect,
-  disconnect,
+  disconnect
 };
