b17e19412b823923c4bac7cf042e5dc135992dab
krthr <twilson@uninorte.edu.co>
2019-12-30 20:12:39 -0500

    Query

--- src/server.js
+++ src/server.js
@@ -1,11 +1,18 @@
 const fs = require("fs");
 const { ApolloServer, gql } = require("apollo-server");
 
+const Query = require("./resolvers/queries");
+
 const typeDefs = gql(
   fs.readFileSync(__dirname.concat("/schema.graphql"), "utf8")
 );
 
+const resolvers = {
+  Query
+};
+
 const server = new ApolloServer({
+  resolvers,
   typeDefs
 });
 
