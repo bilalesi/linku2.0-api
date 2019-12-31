ffe1fefc4a5d9a088c3ecf40741cfd4a50981c99
krthr <twilson@uninorte.edu.co>
2019-12-31 16:30:03 -0500

    getGroupQueue

--- src/services/queue.js
+++ src/services/queue.js
@@ -5,52 +5,65 @@ const { Group, Subject } = require('../models');
 
 const { getAllDepartments, getGroupsByDepartment } = require('./scraper');
 
-const updateDepartmentGroupsQueue = new Queue(
-  'update departments groups',
-  redis.url
-);
-
+const getGroupQueue = new Queue('groups', redis.url);
+const getDepartmentGroupsQueue = new Queue('get departments groups', redis.url);
 const getDepartamentsQueue = new Queue('get departments', redis.url);
 
 // Clean all the jobs in both queues
 for (const status of ['active', 'completed', 'delayed', 'failed', 'wait']) {
-  updateDepartmentGroupsQueue.clean(1000, status);
+  getGroupQueue.clean(1000, status);
+  getDepartmentGroupsQueue.clean(1000, status);
   getDepartamentsQueue.clean(1000, status);
 }
+
+getGroupQueue.process(async job => {
+  const group = job.data;
+
+  const { code, number } = group.subject;
+
+  const subject = await Subject.findOneAndUpdate(
+    {
+      code,
+      number
+    },
+    group.subject,
+    {
+      upsert: true,
+      new: true,
+      setDefaultsOnInsert: true
+    }
+  );
+
+  Object.assign(group, {
+    subject
+  });
+
+  await Group.findOneAndUpdate(
+    {
+      nrc: group.nrc
+    },
+    group,
+    {
+      upsert: true,
+      new: true,
+      setDefaultsOnInsert: true
+    }
+  );
+});
 
 /**
  * Get information of the groups of a department
  */
-updateDepartmentGroupsQueue.process(async job => {
+getDepartmentGroupsQueue.process(async job => {
   const { code } = job.data;
   const groups = await getGroupsByDepartment(code);
 
   job.progress(50);
 
-  await Promise.all(groups.map(async (group) => {
-    const { code, number } = group.subject;
-    const subject = await Subject.findOneAndUpdate({
-      code,
-      number
-    }, group.subject, {
-      upsert: true,
-      new: true,
-      setDefaultsOnInsert: true
-    });
-
-    Object.assign(group, {
-      subject
-    });
-
-    await Group.findOneAndUpdate({
-      nrc: group.nrc,
-    }, group, {
-      upsert: true,
-      new: true,
-      setDefaultsOnInsert: true
-    });
-  }));
-
+  for (const group of groups) {
+    getGroupQueue.add(group);
+    job.progress(((i + 1) / groups.length) * 100);
+  }
 
   job.progress(100);
 });
@@ -62,14 +75,15 @@ getDepartamentsQueue.process(2, async job => {
   const departments = await getAllDepartments();
 
   departments.map((department, i) => {
-    updateDepartmentGroupsQueue.add(department);
+    getDepartmentGroupsQueue.add(department);
     job.progress(((i + 1) / departments.length) * 100);
   });
 });
 
-setQueues([getDepartamentsQueue, updateDepartmentGroupsQueue]);
+setQueues([getGroupQueue, getDepartmentGroupsQueue, getDepartamentsQueue]);
 
 module.exports = {
-  getDepartamentsQueue,
-  updateDepartmentGroupsQueue
+  getGroupQueue,
+  getDepartmentGroupsQueue,
+  getDepartamentsQueue
 };
